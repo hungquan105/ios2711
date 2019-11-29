d63510c46fb7927006b8e55db303957a0dca56f3
Quân <hungquan105@gmail.com>
2019-11-29 22:59:54 +0700

    2911

---
+++ Buoi02/Buoi02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buoi02
+//
+//  Created by Quân on 11/29/19.
+//  Copyright © 2019 QuanNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

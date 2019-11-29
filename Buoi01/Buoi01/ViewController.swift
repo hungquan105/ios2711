ecaaf8276faa6a7c64523c9b24d88557f608bad8
Quân <hungquan105@gmail.com>
2019-11-29 19:51:37 +0700

    first commit

---
+++ Buoi01/Buoi01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buoi01
+//
+//  Created by Quân on 11/27/19.
+//  Copyright © 2019 QuanNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Outlet
+    @IBOutlet weak var lbName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        lbName.text = "Quan Nguyen"
+    }
+
+    // Action
+
+}
+

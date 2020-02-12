6d87f5a4298ac761886cbd8738401ab284b1ae83
Quân <hungquan105@gmail.com>
2020-02-13 00:08:37 +0700

    Code.swift

---
+++ CodeViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ios0709
+//
+//  Created by Quân on 10/19/19.
+//  Copyright © 2019 QuanNguyen. All rights reserved.
+//
+
+import UIKit
+
+class CodeViewController: UIViewController {
+
+    //MARK: IBOUTLETS
+    
+    //MARK: OTHER VARIABLES
+    
+    //MARK: VIEW LIFE CYCLE
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupVar()
+        callAPI()
+    }
+    
+    //MARK: - SETUP VAR
+    func setupVar() {
+        
+    }
+    
+    //MARK: - SETUP UI
+    func setupUI() {
+        
+    }
+    
+    //MARK: - CALL API
+    func callAPI() {
+        
+        fillData()
+    }
+    
+    //MARK: - FILL AND BIND DATA
+    func fillData() {
+        
+    }
+    
+    //MARK: - BUTTON ACTIONS
+
+
+}
+

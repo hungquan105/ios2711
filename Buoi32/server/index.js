2d65da23b12515651245df37d5d356f7d431b5b7
Quân <hungquan105@gmail.com>
2020-02-21 21:51:50 +0700

    Buoi32

---
+++ Buoi32/server/index.js
@@ -0,0 +1,18 @@
+// express
+const express = require('express');
+const app = express();
+// socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    console.log(socket.id + ' connected');
+    // server se lang nghe (on) - data
+    // socket.on()
+        // gui cho tat ca tru nguoi gui
+            // socket.broadcast.emit()
+        // gui cho chinh minh
+            // socket.emit
+});
+
+server.listen(3001);
